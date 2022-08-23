898ee0a26a7f313e81b04ae65120b7f1bd2c66ff
MRJAHID <mrjahid350@gmail.com>
2022-08-23 14:53:10 +0600

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import { ShoppingBasket, Search } from '@mui/icons-material';
+
+const Header = () => {
+   return (
+      <div className="Header">
+         {/* Logo */}
+         <Link to="/">
+            <div className="Header-logo">
+               <img
+                  src="https://mikekitko.com/wp-content/uploads/2019/10/amazon-logo-white-768x232.png"
+                  alt="Logo"
+               />
+            </div>
+         </Link>
+         {/* Address */}
+         <div className="Header-optionAddress">
+            {/* icon */}
+            <div className="Header-option">
+               <span className="Header-optionLineOne">Hello,</span>
+               <span className="Header-optionLineTwo">Select your address</span>
+            </div>
+         </div>
+         {/* Search */}
+         <div className="Header-search">
+            <input className="Header-searchInput" type="text" />
+            <div className="Header-searchIconContainer">
+               <Search />
+            </div>
+         </div>
+         <div className="Header-navItems">
+            {/* Login name */}
+            <div className="Header-option">
+               <span className="Header-optionLineOne">Hello, Nazariy</span>
+               <span className="Header-optionLineTwo">Account & Lsits</span>
+            </div>
+            {/* Orders */}
+            <div className="Header-option">
+               <span className="Header-optionLineOne">Returns</span>
+               <span className="Header-optionLineTwo">& Orders</span>
+            </div>
+            {/* Cart */}
+            <Link to="/cart">
+               <div className="Header-optionCart">
+                  <ShoppingBasket />
+                  <span className="Header-cartCount">getCount</span>
+               </div>
+            </Link>
+         </div>
+      </div>
+   );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWM4k82XSsKZmG6qK_x3yjx6ewxUW7uYs",
+    authDomain: "react-9ede0.firebaseapp.com",
+    projectId: "react-9ede0",
+    storageBucket: "react-9ede0.appspot.com",
+    messagingSenderId: "225295611227",
+    appId: "1:225295611227:web:130f4a1fd2eb239f99567e"
+};
